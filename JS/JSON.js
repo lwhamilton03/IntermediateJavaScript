ae4a855cf0f4cf67f7daebe0c217c6c4b0f40e8d
lwhamilton03 <lucy.hamilton@academytrainee.com>
2018-09-05 12:36:38 +0100

    JSON

---
+++ JS/JSON.js
@@ -0,0 +1,43 @@
+function practiceJSON()
+{
+    var requestURL = "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json";
+
+    var requestArb = new XMLHttpRequest(); 
+
+    requestArb.open('GET', requestURL);
+    requestArb.responseType = 'JSON';
+    requestArb.send(); 
+
+    requestArb.onload = function () 
+        {
+            var superHeroes = requestArb.response; 
+            
+            
+
+            superHeroes = JSON.parse(superHeroes); 
+            
+           // for(i = 0; i < superHeroes.length; i++)
+                
+            console.log(superHeroes);
+            document.write(superHeroes.squadName + "<br/>");   
+            for(random in superHeroes.members)
+                {
+                    document.write("<br/> Name: " + superHeroes.members[random].name);
+                    document.write("<br/> Age: " + superHeroes.members[random].age);
+                    document.write("<br/>");
+                }
+            document.write("<br> Secret Base: " + superHeroes.secretBase + "... TOP SECRET!"); 
+            document.write("<br/> The squad formed in " + superHeroes.formed); 
+            
+            for(random3 in superHeroes.members)
+                {
+                    for(random2 in superHeroes.members[random3].powers)
+                        {
+                            document.write("<br/>" + superHeroes.members[random].powers[random2]);
+                            
+                        }
+                        document.write("<br/>");
+                }; 
+        };
+     
+}
